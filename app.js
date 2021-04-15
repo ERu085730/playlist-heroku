69c28abf23afc8fd454fdd479f31209b36329241
ERu085730 <ss86262549@gmail.com>
2021-04-15 11:14:07 +0800

    re4

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const listRoutes = require('./routes/listRoute')
+const PORT = process.env.PORT || 5000
+const cool = require('cool-ascii-faces');
 
 const { render } = require('ejs');
 
@@ -12,7 +14,7 @@ const app = express();
 const dbURI = 'mongodb+srv://test:1234@playlist.vh6ku.mongodb.net/PlayList?retryWrites=true&w=majority';
 
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then((result) => app.listen(3000))
+    .then((result) => app.listen(PORT))
     .catch((err) => console.log(err));
 
 // register view engine
@@ -25,6 +27,10 @@ app.use(morgan('dev'));
 
 app.get('/', (req, res) =>{
     res.redirect('/lists');
+});
+
+app.get('/cool', (req, res) =>{
+    res.send(cool());
 });
 
 app.use('/lists', listRoutes);
